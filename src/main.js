93c3da5944d9da676e6bf2943b00805d1f6c2c87
jiyuning <1624146711@qq.com>
2019-12-19 10:15:22 +0800

    安装elementUI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElenmenrUI from 'element-ui'// 引入模块
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+Vue.use(ElenmenrUI)
 
 Vue.config.productionTip = false
 
