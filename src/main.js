c9e34c52b8124618247baff04780c79f59906199
jiyuning <1624146711@qq.com>
2019-12-19 10:48:44 +0800

    创建登录，主页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import ElenmenrUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './styles/index.less'// 引入初始化css文件
+
 Vue.use(ElenmenrUI)
 
 Vue.config.productionTip = false
