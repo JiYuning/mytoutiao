113c3a3a55e5a3d58c3f0b5e83fd37540eea0d3f
jiyuning <1624146711@qq.com>
2019-12-19 10:25:36 +0800

    准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElenmenrUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+import './styles/index.less'// 引入初始化css文件
 Vue.use(ElenmenrUI)
 
 Vue.config.productionTip = false
