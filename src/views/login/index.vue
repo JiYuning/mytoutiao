dffcca03ef8797d72cb2d82a8a6d221c402f9b80
jiyuning <1624146711@qq.com>
2019-12-21 20:55:56 +0800

    登录成功

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,18 +6,18 @@
         <hr>
       </div>
       <el-form ref="myForm" :model="loginFrom" :rules="loginReturn">
-        <el-form-item prop="phone">
-          <el-input v-model="loginFrom.phone" placeholder="请输入手机号"></el-input>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginFrom.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item prop="code">
           <el-input v-model="loginFrom.code" placeholder="请输入验证码" style="width:70%"></el-input>
           <el-button plain style="float:right">获取验证码</el-button>
         </el-form-item>
         <el-form-item prop="check">
-          <el-checkbox v-model="loginFrom.check">我已阅读并同意无条件被坑</el-checkbox>
+          <el-checkbox v-model="loginFrom.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="btn" @click="submitLogin('myForm')">登录</el-button>
+          <el-button type="primary" class="btn" @click="submitLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,24 +29,24 @@ export default {
   data () {
     return {
       loginFrom: {
-        phone: '', // 手机号
-        code: '', // 验证码
-        check: false// 是否勾选
+        mobile: '',
+        code: '',
+        check: false
       },
       loginReturn: {
-        phone: [
-          { reqired: true, message: '请输入你的手机号' },
+        mobile: [
+          { required: true, message: '请输入你的手机号' },
           { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }
         ],
         code: [
-          { reqired: true, message: '请输入你的验证码' },
+          { required: true, message: '请输入你的验证码' },
           { pattern: /^\d{6}$/, message: '验证码不正确' }
         ],
         check: [{ validator: function (rule, value, callback) {
           if (value) {
             callback()
           } else {
-            callback(new Error('请勾选我们的霸王条约'))
+            callback(new Error('请勾选'))
           }
         }
         }]
@@ -55,7 +55,7 @@ export default {
   },
   methods: {
     submitLogin () {
-      this.$refs.myForm.validate(function (ok) {
+      this.$refs.myForm.validate((ok) => {
         if (ok) {
           this.$axios({
             url: '/authorizations',
@@ -63,7 +63,10 @@ export default {
             data: this.loginFrom
           }).then(res => {
             window.localStorage.setItem('user-token', res.data.data.token)
-          }).catch(() => {})
+            this.$router.push('/home')
+          }).catch(err => {
+            console.log(err)
+          })
         }
       })
     }
