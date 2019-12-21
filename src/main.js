17ccb91c3f3c68d5c8becf306c5791305d183d87
jiyuning <1624146711@qq.com>
2019-12-21 10:31:05 +0800

    登录页完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElenmenrUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './styles/index.less'// 引入初始化css文件
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 赋值给全局对象
 
 Vue.use(ElenmenrUI)
 
