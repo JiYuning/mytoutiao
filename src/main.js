572593562ebcfbe7b1b9743158ec31b968dc55cd
jiyuning <1624146711@qq.com>
2019-12-21 10:34:37 +0800

    安装axios，并配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElenmenrUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './styles/index.less'// 引入初始化css文件
-import axios from 'axios'
+import axios from 'axios'// 安装axios，配置全局使用
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios // 赋值给全局对象
 
