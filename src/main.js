725bcba9ad4248d0b626af0e0575a577401860a4
jiyuning <1624146711@qq.com>
2019-12-24 11:51:02 +0800

    设置导航守卫

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './perimission'
 import router from './router'
 import ElenmenrUI from 'element-ui'// 引入模块
 import Component from './components'
