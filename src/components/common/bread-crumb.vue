19a57ce6bfabf9c9aa19e07b2634a1b83a968e43
jiyuning <1624146711@qq.com>
2019-12-25 10:34:38 +0800

    面包屑封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-breadcrumb>
+        <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+        <el-breadcrumb-itemb>
+            <slot name='title'></slot>
+        </el-breadcrumb-itemb>
+    </el-breadcrumb>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './home/layout-aside'
 import layoutTop from './home/layout-top'
+import breadCrumb from './common/bread-crumb'
 
 export default {
   install (Vue) {
-    Vue.component('layout-aside', layoutAside)
-    Vue.component('layout-top', layoutTop)
+    Vue.component('layout-aside', layoutAside)// 注册全局组件
+    Vue.component('layout-top', layoutTop)// 注册全局组件
+    Vue.component('bread-crumb', breadCrumb)// 注册全局面包屑
   }
 }
