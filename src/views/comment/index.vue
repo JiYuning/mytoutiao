5fb995ace2bfafdd2fd936588450cd19a54df78f
jiyuning <1624146711@qq.com>
2019-12-25 11:15:14 +0800

    评论列表搭建页面

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,15 +1,23 @@
 <template>
-<div>
-    评论
-</div>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">
+          评论管理
+      </template>
+    </bread-crumb>
+    <el-table>
+      <el-table-column label="标题" width="600"></el-table-column>
+      <el-table-column label="评论状态"></el-table-column>
+      <el-table-column label="总评论数"></el-table-column>
+      <el-table-column label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
